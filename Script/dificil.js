d8022fba67cf411c2df52d84b8d748f23f0bf46e
chrisemm96 <84466794+chrisemm96@users.noreply.github.com>
2021-07-10 16:40:07 -0500

    SE agrega el proyecto

---
+++ Script/dificil.js
@@ -0,0 +1,27 @@
+document.getElementById('player').addEventListener("mouseover",sumarPuntos);
+
+puntos =0;
+tiempo =25;
+necesarios=50;
+function sumarPuntos(){
+  puntos++;
+  document.getElementById("puntos").innerHTML="Puntos:<b> "+puntos+"/"+necesarios+"</b>";
+  randNum =Math.round(Math.random()*500);
+  randNum2 =Math.round(Math.random()*500);
+  document.getElementById("player").style.marginTop = randNum + "px";
+  document.getElementById("player").style.marginLeft = randNum2 + "px";
+  if(puntos==necesarios){
+    alert("ganaste");
+  }
+}
+function restarTiempo(){
+  tiempo--;
+  document.getElementById("tiempo").innerHTML= "&nbsp;&nbsp;&nbsp;Tiempo: "+tiempo;
+  if(tiempo==0){
+    alert("perdiste");
+    tiempo=0;
+    puntos=0;
+  }
+}
+
+setInterval(restarTiempo,1000);
